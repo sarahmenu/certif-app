b342a474348dcd50933afbb1fed89de19b5098de
Sarah Menu <sarah.menu@gmail.com>
2022-06-23 17:30:09 +0200

    Config flatpicker

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,8 +3,8 @@ class Room < ApplicationRecord
   has_one_attached :photo
   has_many :bookings, dependent: :destroy
 
-  validates :price_per_day, numericality: { greater_than: 0 }
-  validates :price_per_day, presence: true
+  validates :price_per_night, numericality: { greater_than: 0 }
+  validates :price_per_night, presence: true
 
   validates :capacity, presence: true
   validates :capacity, numericality: { greater_than: 0, only_integer: true }
