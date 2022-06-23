9d73eb8f4176876727ca007f2f967581a27d6630
Sarah Menu <sarah.menu@gmail.com>
2022-06-23 14:22:30 +0200

    Initial setup

---
+++ db/migrate/20220623121634_create_bookings.rb
@@ -0,0 +1,12 @@
+class CreateBookings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :bookings do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :room, null: false, foreign_key: true
+      t.date :starts_at
+      t.date :ends_at
+
+      t.timestamps
+    end
+  end
+end
