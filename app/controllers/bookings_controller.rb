a3b6832c0b0503c421f7808c46f88b0e020192e3
Sarah Menu <sarah.menu@gmail.com>
2022-06-23 16:46:18 +0200

    Done booking feature

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,25 @@
 class BookingsController < ApplicationController
+
+  def index
+    @bookings = Booking.where(user: current_user)
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @room = Room.find(params[:room_id])
+    @user = current_user
+    @booking.room = @room
+    @booking.user = @user
+    if @booking.save
+      redirect_to room_path(@room), notice: "Congratulations ! Your booking has come through!"
+    else
+      redirect_to room_path(@room), alert: "Error ! Your booking could not be processed."
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starts_at, :ends_at)
+  end
 end
